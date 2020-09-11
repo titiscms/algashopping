981b43fc45ba139d9c569b03fb6975671aae0f4a
titiscms <titisbackup@gmail.com>
2020-09-11 01:12:52 -0300

    add linechart shared component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { Wrapper, Container } from './App.styles'
 import AppHeader from '../AppHeader'
 import AppContainer from '../AppContainer'
-import { Wrapper, Container } from './App.styles'
 import Checkbox from '../../shared/Checkbox/Checkbox'
+import LineChart from '../../shared/LineChart/LineChart'
 
 function App() {
     const [ lettuce, setLettuce ] = useState(true)
     const [ rice, setRice ] = useState(false)
+    const [ healthy, setHealthy ] = useState(20)
+
+    const colors = [ '#62cbc6', '#00abad', '#00858c', '#006073', '#004d61' ]
+
+    useEffect(function() {
+        setTimeout(() => {
+            setHealthy(80)
+        }, 5000)
+    },[])
 
     return <Wrapper>
         <Container>
@@ -32,6 +42,31 @@ function App() {
                 </div>}
                 right={<div>
                     estatísticas
+
+                    <LineChart
+                        color={colors[0]}
+                        title="saudável"
+                        percentage={healthy}
+                    />
+
+
+                    <LineChart
+                        color={colors[1]}
+                        title="não tão saudável assim"
+                        percentage={45}
+                    />
+
+                    <LineChart
+                        color={colors[2]}
+                        title="limpeza"
+                        percentage={35}
+                    />
+
+                    <LineChart
+                        color={colors[3]}
+                        title="outros"
+                        percentage={15}
+                    />
                 </div>}
             />
         </Container>
