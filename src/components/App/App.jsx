a865f8c841fec853a6aa193bfa1bce82a4afd84c
titiscms <titisbackup@gmail.com>
2020-09-10 23:59:28 -0300

    add base layout

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import AppHeader from '../AppHeader'
+import AppContainer from '../AppContainer'
 import { Wrapper, Container } from './App.styles'
 
 function App() {
     return <Wrapper>
         <Container>
-
+            <AppHeader />
+            <AppContainer 
+                left={<div style={{ backgroundColor: 'blue' }}>
+                    produtos disponíveis
+                </div>}
+                middle={<div style={{ backgroundColor: 'green'  }}>
+                    sua lista de compras
+                </div>}
+                right={<div style={{ backgroundColor: 'red' }}>
+                    estatísticas
+                </div>}
+            />
         </Container>
     </Wrapper>
 }
---
+++ src/components/AppHeader/AppHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Wrapper } from './AppHeader.styles'
+
+function AppHeader() {
+    return <Wrapper>
+        alga <span>Shopping</span>
+    </Wrapper>
+}
+
+export default AppHeader
+
