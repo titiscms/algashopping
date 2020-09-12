2fa4f8c23432b6beb2e9ff896e7887aa95d46318
titiscms <titisbackup@gmail.com>
2020-09-12 16:16:37 -0300

    add base functionality

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,53 +2,58 @@ import React, { useState, useEffect } from 'react'
 import { Wrapper, Container } from './App.styles'
 import AppHeader from '../AppHeader'
 import AppContainer from '../AppContainer'
-import Checkbox from '../../shared/Checkbox/Checkbox'
 import LineChart from '../../shared/LineChart/LineChart'
+import ShoppingList from '../ShoppingList'
+import productsMock from '../../mocks/products.json'
 
 function App() {
-    const [ lettuce, setLettuce ] = useState(true)
-    const [ rice, setRice ] = useState(false)
-    const [ healthy, setHealthy ] = useState(20)
-
     const colors = [ '#62cbc6', '#00abad', '#00858c', '#006073', '#004d61' ]
+    
+    const [products, setProducts] = useState(productsMock.products)
+    const [selectedProducts, setSelectedProducts] = useState([])
+    
+    useEffect(() => {
+        const newSelectedProducts = products.filter(product => product.checked)
 
-    useEffect(function() {
-        setTimeout(() => {
-            setHealthy(80)
-        }, 5000)
-    },[])
+        setSelectedProducts(newSelectedProducts)
+    }, [products])
+
+    function handleToggle (id) {
+        const newProducts = products.map(product => 
+
+            product.id === id ? { ...product, checked: !product.checked } : product
+
+        )
+
+        setProducts(newProducts)
+    }
 
     return <Wrapper>
         <Container>
             <AppHeader />
             <AppContainer 
-                left={<div>
-                    produtos disponíveis
+                left={
+                    <ShoppingList 
+                        title="produtos disponíveis"
+                        products={products}
+                        onToggle={handleToggle}
+                    />}
 
-                    <Checkbox 
-                        value={lettuce} 
-                        title="Alface"
-                        onClick={() => setLettuce(!lettuce)}
-                    />
-                </div>}
-                middle={<div>
-                    sua lista de compras
+                middle={
+                    <ShoppingList 
+                        title="sua lista de compras"
+                        products={selectedProducts}
+                        onToggle={handleToggle}
+                    />}
 
-                    <Checkbox
-                        value={rice} 
-                        title="Arroz"
-                        onClick={() => setRice(!rice)}
-                    />
-                </div>}
                 right={<div>
                     estatísticas
 
                     <LineChart
                         color={colors[0]}
                         title="saudável"
-                        percentage={healthy}
+                        percentage={80}
                     />
-
 
                     <LineChart
                         color={colors[1]}
