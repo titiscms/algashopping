276f336461ac2dba32322583cfe2e5c08c58e441
titiscms <titisbackup@gmail.com>
2020-09-16 22:44:41 -0300

    install basic redux

---
+++ src/store/Calculator/Calculator.actions.js
@@ -0,0 +1,13 @@
+export function sum () {
+    return {
+        type: 'SUM',
+        payload: [a, b]
+    }
+}
+
+export function subtract () {
+    return {
+        type: 'SUBTRACT',
+        payload: [a, b]
+    }
+}
